534dac69bf18fc7f41a1f221b96316955984f895
Minwoo Park <tebica@gmail.com>
2019-02-20 04:38:52 +0900

    update to avoid 2 css inclusion, edit title, contents align etc

---
+++ twentynineteen-child/footer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
+		<div class="site-info">
+			<?php if ( has_nav_menu( 'footer' ) ) : ?>
+				<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'twentynineteen' ); ?>">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'footer',
+							'menu_class'     => 'footer-menu',
+							'depth'          => 1,
+						)
+					);
+					?>
+				</nav><!-- .footer-navigation -->
+			<?php endif; ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- twentynineteen-child/functions.php
+++ twentynineteen-child/functions.php
@@ -7,7 +7,7 @@ function enqueue_wp_child_theme()
 {
 	wp_enqueue_style('parent-css', get_template_directory_uri().'/style.css' );
 
-	wp_enqueue_style('child-css', get_stylesheet_uri());
+	//wp_enqueue_style('child-css', get_stylesheet_uri());
 
 	wp_enqueue_script('child-js', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0', true );
 }
